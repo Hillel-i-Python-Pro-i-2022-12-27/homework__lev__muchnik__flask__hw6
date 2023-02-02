7367147b3b83b22719a3f4448cebfd7ba7d30996
Лев Мучник <lionrav19@gmail.com>
2023-02-02 22:10:44 +0200

    fix application

--- application/files_example/read_text_file.py
+++ application/files_example/read_text_file.py
@@ -2,12 +2,13 @@ from application.config.paths import FILES_INPUT_PATH
 from application.logging.loggers import get_core_logger
 
 
-def read_text_file(name_file: str = None) -> None:
+def read_text_file(name_file: str = None) -> str:
     logger = get_core_logger()
     path_to_file = FILES_INPUT_PATH.joinpath(f"{name_file}.txt")
     logger.info(f"Path to file: file://{path_to_file}")
     file_text = path_to_file.read_text()
-    print(file_text)
+    # print(file_text)
+    return f"{file_text}"
 
 
 if __name__ == "__main__":
